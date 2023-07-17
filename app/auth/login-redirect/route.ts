35e9bdaad2f0e71fa853f6cec87f6334b63cb54c
Netanel Gilad <netanelg@wix.com>
2023-07-17 17:24:21 +0300

    use the new `prompt` parameter in `getAuthUrl` for silent login (#119)
    * use the new `prompt` parameter in `getAuthUrl` for silent login
    
    * use a const

--- app/auth/login-redirect/route.ts
+++ app/auth/login-redirect/route.ts
@@ -3,6 +3,7 @@ import {
   AUTH_CALLBACK_PATHNAME,
   AUTH_LOGIN_CALLBACK_PARAM,
   OAUTH_COOKIE_STATE,
+  PROMPT_QUERY_PARAM,
 } from '@app/model/auth/auth.const';
 import { getServerWixClient } from '@app/model/auth/wix-client.server';
 import { getRequestUrl } from '@app/model/server-utils';
@@ -17,6 +18,8 @@ export async function GET(request: NextRequest) {
   const requestUrl = getRequestUrl(request);
   const { searchParams } = new URL(requestUrl);
   const originalUrl = searchParams.get(AUTH_LOGIN_CALLBACK_PARAM);
+  const prompt =
+    (searchParams.get(PROMPT_QUERY_PARAM) as 'login' | 'none') ?? 'login';
   if (!originalUrl) {
     throw new Error(
       `${AUTH_LOGIN_CALLBACK_PARAM} is required for login redirect`
@@ -24,7 +27,9 @@ export async function GET(request: NextRequest) {
   }
   const redirectUrl = new URL(AUTH_CALLBACK_PATHNAME, requestUrl).toString();
   const oauthData = wixClient!.auth.generateOAuthData(redirectUrl, originalUrl);
-  const { authUrl } = await wixClient!.auth.getAuthUrl(oauthData);
+  const { authUrl } = await wixClient!.auth.getAuthUrl(oauthData, {
+    prompt,
+  });
   const response = NextResponse.redirect(authUrl);
   response.cookies.set({
     name: OAUTH_COOKIE_STATE,
